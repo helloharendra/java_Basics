1f0a22e65036045b1237ce1df300b80c03ab261a
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-12-24 09:08:37 +0530

    commit

---
+++ JavaCustructor.java
@@ -0,0 +1,10 @@
+public class JavaCustructor {
+    JavaCustructor(){
+        System.out.println("hello i am using cunstructor here");
+    }
+    public static void main(String[] args) {
+        new JavaCustructor(); //calling the constructor
+    
+    }
+
+}
