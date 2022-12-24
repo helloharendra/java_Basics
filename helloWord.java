79e336728c62eacfe12a354ea192fe288a137379
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-12-24 09:08:50 +0530

    commit2

--- helloWord.java
+++ helloWord.java
@@ -1,8 +1,12 @@
-class helloWord{
-    public static void main (  String[] agrs){
-        System.out.println("hello World");
+//read carefully , you will be able to crate a class and method after reading and practice it 
+
+public class helloWord // creating public  class 
+{  // opening of class body
+    public static void main (  String[] agrs) // creating main method 
+    { // opening of main method body
+        System.out.println("hello World"); //print message
         
 
-    }
+    }// closing of main method body
 
-}
+} // closing of class body
