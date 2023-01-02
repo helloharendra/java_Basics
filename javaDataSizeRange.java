52a2df2dc99ae3304a48a64f6bfa1e44a935b071
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-01-02 14:44:14 +0530

    max size of byte

--- javaDataSizeRange.java
+++ javaDataSizeRange.java
@@ -2,5 +2,9 @@ public class javaDataSizeRange{
     public static void main(String[] args) {
         System.out.println("MIN Range of integer is " + Integer.MIN_VALUE);
         System.out.println("Max Range of integer is  " + Integer.MAX_VALUE);
+        System.out.println("Max Byte  " + Integer.BYTES);
+        System.out.println("min Range of short is  " + Byte.MIN_VALUE);
+        System.out.println("Max Range of short is  " + Byte.MAX_VALUE);
+
     }
 }
