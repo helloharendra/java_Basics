b75a8c33764c403c53de82d516854bfc050b4874
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-01-02 14:38:36 +0530

    interger min and max value

---
+++ javaDataSizeRange.java
@@ -0,0 +1,6 @@
+public class javaDataSizeRange{
+    public static void main(String[] args) {
+        System.out.println("MIN Range of integer is " + Integer.MIN_VALUE);
+        System.out.println("Max Range of integer is  " + Integer.MAX_VALUE);
+    }
+}
