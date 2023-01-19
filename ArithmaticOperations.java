5aba62640a7c480ad4d1060fd9bc97662f8968bc
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-20 00:03:51 +0530

    arithmatic

---
+++ ArithmaticOperations.java
@@ -0,0 +1,12 @@
+class ArithmaticOperations{
+    public static void main(String[] args) {
+        int a=10,b=3;
+        int c=a/b;
+        float d=a/b;
+        int r=a%b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(r);
+
+    }
+}
