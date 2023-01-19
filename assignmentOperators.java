2fd18f8c276e113e0a63cd4be203e6e80bc4ef52
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-20 01:32:48 +0530

    commit

---
+++ assignmentOperators.java
@@ -0,0 +1,13 @@
+public class assignmentOperators {
+    public static void main(String[] args) {
+        int a=10;
+        a++;
+        int b=a++;
+        int d=++a;
+        System.out.println("value of a is "+a);
+        System.out.println("value of b is "+b);
+        System.out.println("value of b is "+d);
+
+    }
+    
+}
