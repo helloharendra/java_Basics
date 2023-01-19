a7043f25f6bdf0b57824b822e821e67e1a1ce0c1
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-20 00:21:55 +0530

    commit

---
+++ areaOfTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class areaOfTriangle {
+    public static void main(String[] args) {
+        float base,height,area;
+
+        System.out.println("Enter base and height");
+        Scanner sc=new Scanner(System.in);
+        base=sc.nextFloat();
+        height=sc.nextFloat();
+
+        area=base*height/2;
+        System.out.println("Area of Triangle is :" +area);
+
+        
+    }
+    
+}
---
+++ areaOftriangle2ndmethod.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class areaOftriangle2ndmethod {
+    public static void main(String[] args) {
+        int a,b,c;
+        float s;
+
+        System.out.println("Enter the value of a , b andc");
+
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        s=(a+b+c)/2f;
+        
+        double area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.println(area);
+
+
+
+
+
+    }
+    
+}
