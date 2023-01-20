e000e3b9fa1216ec8a00d13c1f72c08b1544e729
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-21 00:26:48 +0530

    java String

---
+++ javaStriing.java
@@ -0,0 +1,18 @@
+public class javaStriing {
+    public static void main(String[] args) {
+        String str="hello harendar";
+
+        String str1=new String("helloharendra"); 
+
+        char ch[]={'a','b','c'};
+
+        String str2=new String(ch);
+
+        System.out.println(str);
+        System.out.println(str1);
+        System.out.println(str2);
+ 
+
+    }
+    
+}
