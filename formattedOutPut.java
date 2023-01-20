d95b3cb6cba12f34c9ae2742aa3c3b8b9e117415
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-21 00:02:20 +0530

    formatted output

---
+++ formattedOutPut.java
@@ -0,0 +1,11 @@
+public class formattedOutPut {
+    public static void main(String[] args) {
+        int a=10;
+        
+        System.out.printf("%d",a); // %d represents integer type of data
+        System.out.println();
+        System.out.printf("%5d",a); // print at 5th place
+        System.out.println();
+    }
+    
+}
