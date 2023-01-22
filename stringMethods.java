95bee921b10b2202555a9ecd8194f09bfb181255
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-22 23:32:54 +0530

    commit

--- stringMethods.java
+++ stringMethods.java
@@ -1,8 +1,8 @@
 class stringMethods{
     public static void main(String[] args) {
         String str1="hellooharendra";
-        System.out.println("String is"+str1);
-        System.out.println(str1.length());
+        System.out.println("String is : "+str1);
+        System.out.println("String Length is : "+str1.length());
 
     }
 }
