8b7b716daab1dcec8d0f6f7340f63bac30a9fb3f
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-22 23:36:29 +0530

    tolowercase

--- stringMethods.java
+++ stringMethods.java
@@ -3,6 +3,8 @@ class stringMethods{
         String str1="hellooharendra";
         System.out.println("String is : "+str1);
         System.out.println("String Length is : "+str1.length());
+        System.out.println(str1 .toUpperCase());
+        System.out.println(str1 .toLowerCase());
 
     }
 }
