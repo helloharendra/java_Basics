ebc5ae78bfa01d366ba39e3ece8a8ec93e8a6225
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-22 23:53:20 +0530

    commit

--- stringMethods.java
+++ stringMethods.java
@@ -1,6 +1,6 @@
 class stringMethods{
     public static void main(String[] args) {
-        String str1="hellooharendra";
+        String str1="     hellooharendra";
         System.out.println("String is : "+str1);
         System.out.println("String Length is : "+str1.length());
         System.out.println(str1 .toUpperCase());
