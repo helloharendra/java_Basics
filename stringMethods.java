fa2931c6f2b282bc0a4d78b69f3d240691589177
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-22 23:32:02 +0530

    string methods

---
+++ stringMethods.java
@@ -0,0 +1,8 @@
+class stringMethods{
+    public static void main(String[] args) {
+        String str1="hellooharendra";
+        System.out.println("String is"+str1);
+        System.out.println(str1.length());
+
+    }
+}
