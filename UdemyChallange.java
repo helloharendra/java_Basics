282ee4d1558a02cc05bc9bfba97318c245cb7737
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-23 16:56:44 +0530

    Udemychallange

--- UdemyChallange.java
+++ UdemyChallange.java
@@ -2,9 +2,18 @@ public class UdemyChallange {
     public static void main(String[] args) {
        String str="programmer@gmail.com";
 
-       System.out.println(str.matches(".*@gmail.com*"));
-        
-    System.out.println(str.substring(0,10));
+       int i=str.indexOf("@");
+
+       String uname=str.substring(0,i);
+       String domain=str.substring(i+1, str.length());
+
+       System.out.println("Username :"+uname);
+
+       System.out.println("Domain :"+domain);
+
+       System.out.println(domain.matches("gmail.com"));
+
+
 
     }
     
