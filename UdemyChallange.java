936c30bc21d4a45c7ef847334b1e9a42705553ee
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-23 16:45:58 +0530

    commit

---
+++ UdemyChallange.java
@@ -0,0 +1,11 @@
+public class UdemyChallange {
+    public static void main(String[] args) {
+       String str="programmer@gmail.com";
+
+       System.out.println(str.matches(".*@gmail.com*"));
+        
+    System.out.println(str.substring(0,10));
+
+    }
+    
+}
