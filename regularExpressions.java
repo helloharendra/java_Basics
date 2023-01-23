d742f4c66c8f9dc7a25569d07bd3d006dfa0c9a2
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-23 16:01:56 +0530

    commit

--- regularExpressions.java
+++ regularExpressions.java
@@ -4,11 +4,13 @@ public class regularExpressions {
         String str1="f";
         String str2="8";
         String str3="abcd";
-
         System.out.println(str1.matches(".")); // . matches only single characters else give false
         System.out.println(str2.matches(".")); // . matches only single characters else give false
         System.out.println(str3.matches(".")); // . matches only single characters else give false
         System.out.println(str1.matches("[abcf]")); // . matches only single characters else give false
+
+        String str="a";
+        System.out.println(str.matches("\\w"));
 
     }
     
