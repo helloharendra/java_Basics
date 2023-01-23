1c508da56a72f2ea6ef14071c1f0fcf01beb1a81
studuhubtutorial <harendraprajapati72@gmail.com>
2023-01-23 22:00:02 +0530

    commit

---
+++ relationalOperators.java
@@ -0,0 +1,15 @@
+public class relationalOperators {
+    public static void main(String[] args) {
+        //conditional statement
+
+        int x=10;
+        if(x>=0){
+            System.out.println("positive");
+        }else{
+            System.out.println("Negative");
+
+
+        }
+    }
+    
+}
